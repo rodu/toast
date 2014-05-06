91a630c64e95b5d446cfb5b91958bead54b5efaa
rodu <greenheron35@gmail.com>
2014-05-06 11:13:37 +0100

    Adds gathering of client info

--- app/models/schemas/testrun.js
+++ app/models/schemas/testrun.js
@@ -8,6 +8,33 @@ var mongoose = require('mongoose'),
             'default': 'ProjectName',
             trim: true
         },
+        clientInfo: {
+            userAgent: {
+                type: String,
+                'default': '',
+                trim: true
+            },
+            browserName: {
+                type: String,
+                'default': '',
+                trim: true
+            },
+            browserVersion: {
+                type: String,
+                'default': '',
+                trim: true
+            },
+            osArch: {
+                type: String,
+                'default': '',
+                trim: true
+            },
+            osFamily: {
+                type: String,
+                'default': 'ProjectName',
+                trim: true
+            }
+        },
         creationDate: {
             type: Date,
             'default': Date.now
